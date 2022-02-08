16b7ffaf2b735fe0b29907701edf7d7813ebdb05
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-02-08 10:12:48 +0100

    ok

--- composables/useTableData.ts
+++ composables/useTableData.ts
@@ -46,18 +46,18 @@ const useTableData = () => {
             }
         },
         {
-            imageUrl: "@/assets/images/companies/commandr.svg",
-            title: "Command+R",
-            subTitle: "cmdr.ai",
-            progress: 35.42,
+            imageUrl: "@/assets/images/companies/hourglass.svg",
+            title: "Hourglass",
+            subTitle: "hourglass.app",
+            progress: 73.37,
             status: "Customer",
             userCount: {
                 enabled: true,
                 value: "+5"
             },
             About: {
-                title: "Data prediction",
-                subTitle: "AI and machine learning data"
+                title: "Productivity app",
+                subTitle: "Time management and productivity"
             }
         },
     ]
